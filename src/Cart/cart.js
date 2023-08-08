12faf9f7331ab9a1dccc50cbdaa1c8da760b812f
Muresan Razvan <razvy9099@yahoo.com>
2023-08-08 17:42:27 +0300

    cart technically functional (almost)

--- src/Cart/cart.js
+++ src/Cart/cart.js
@@ -1,37 +1,34 @@
 import React from "react";
 
 export class Cart extends React.Component {
-    constructor(authKey) {
+    constructor(authKey, setCart) {
         super(authKey);
         this.cartID = "64ca2f11625dd"
         this.cart = null
-        this.state = {
-            modified: false
-        }
         this.authKey = authKey
+        this.setCart = setCart
+    }
+
+    state = {
+        modified: false
+    }
+
+    modifying = (modified) => {
+        this.setState({
+            modified: !modified
+        })
     }
 
     async getCart() {
-        // because I may want to get the cart when it's already fetched
-        if (this.cart)
-            return this.cart
-        // because initially the cart is null
-        else {
-            // local cart may already be saved in local storage
-            const localCart = localStorage.getItem("cart")
-            if (localCart != null && !this.state.modified)
-                return JSON.parse(localCart)
-        }
-
         // fetch the cart
         this.cart = await fetch(`https://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/${this.cartID}`, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json',  'Internship-Auth': `${this.authKey}`}
         })
             .then(res => res.json())
-        localStorage.setItem("cart", JSON.stringify(this.cart))
-        this.setState({ modified: !this.state.modified})
-        return this.cart
+        this.setCart(this.cart)
+        this.modifying(this.state.modified)
+
     }
 
     getStringOfProducts(products) {
@@ -61,8 +58,8 @@ export class Cart extends React.Component {
                     products: requestedProducts
                 })
             }).then(res => res.json())
-            this.setState({ modified: true })
-            this.cart = this.getCart()
+            this.modifying(this.state.modified)
+            this.getCart(this.setCart)
         }
 
         if (toDelete.length > 0) {
@@ -70,8 +67,8 @@ export class Cart extends React.Component {
                 method: 'DELETE',
                 headers: {'Content-Type': 'application/json', 'Internship-Auth': `${this.authKey}`}
             }).then(res => res.json())
-            this.setState({ modified: true })
-            this.cart = this.getCart()
+            this.modifying(this.state.modified)
+            this.getCart(this.setCart)
         }
     }
 }
--- src/Cart/cartPage.js
+++ src/Cart/cartPage.js
@@ -3,46 +3,47 @@ import {useAuth} from "../Login/login";
 import {useEffect, useState} from "react";
 
 export default function CartPage() {
-    const { authKey } = useAuth()
-    const cartObj = new Cart(authKey.data.token)
-    const [cart, setCart ] = useState()
+    const {authKey} = useAuth()
+    const [cart, setCart] = useState(null)
+    const cartObj = new Cart(authKey.data.token, setCart)
 
-    useEffect(() => {
-        setCart(cartObj.getCart())
-    }, []);
+    useEffect( () => {
+        cartObj.getCart()
+    }, [])
 
-    const Product = ({ product }) => {
+    const Product = ({product}) => {
         return (
             <div className={"cart-page-product"}>
-                <img src={product.thumbnail} className={"cart-product-img"} />
+                <img src={product.thumbnail} className={"cart-product-img"}/>
                 <p>{product.title}</p>
                 <p>${Math.floor(product.discountedPrice)}</p>
                 <div className={"cart-page-quantity"}>
-                    <button className={"q-btn"} id={`minus-${product.id}`} onClick={handleMinus}>
-                        <i className={"fa fa-minus"} aria-hidden={"true"} />
+                    <button className={"q-btn"} id={`minus-${product.id}`} onClick={(e) => { handleMinus(product); cartObj.getCart() }}>
+                        <i className={"fa fa-minus"} aria-hidden={"true"}/>
                     </button>
                     <p>{product.quantity}</p>
-                    <button className={"q-btn"} id={`plus-${product.id}`} onClick={handlePlus}>
-                        <i className={"fa fa-plus"} aria-hidden={"true"} />
+                    <button className={"q-btn"} id={`plus-${product.id}`} onClick={(e) => { handlePlus(product); cartObj.getCart() }}>
+                        <i className={"fa fa-plus"} aria-hidden={"true"}/>
                     </button>
                 </div>
             </div>
         )
     }
 
-    function handlePlus(product) {
-        cartObj.updateCart([{id: product.id, quantity: 1}])
+    async function handlePlus(product) {
+        await cartObj.updateCart([{id: Number(product.id), quantity: 1}])
     }
 
-    function handleMinus(product) {
-        cartObj.updateCart([{id: product.id, quantity: -1}])
+    async function handleMinus(product) {
+        await cartObj.updateCart([{id: Number(product.id), quantity: -1}])
     }
+
     if (cart)
         return (
             <main className={"cart-page-main"}>
-                    <div id={"products"} className={"cart-page-quantity"}>
+                <div id={"products"} className={"cart-page-quantity"}>
                     {
-                        cart.products.map((product) => <Product product={product} /> )
+                        cart.products.map((product) => <Product key={product.id} product={product}/>)
                     }
                 </div>
                 <p>Total: ${Math.floor(cart.discountTotal)}</p>
